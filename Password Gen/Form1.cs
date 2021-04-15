9429ee1fb057565957835d41e561cd3bb4619c98
sam40072 <samger04@gmail.com>
2021-04-14 22:41:12 -0400

    Add files via upload

---
+++ Password Gen/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Password_Gen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            try
+            {
+                if (textBox1.Text == "")
+                {
+                    
+                    f.Show();
+                }
+                textBox2.Text = quack.randomstring(Convert.ToInt32(textBox1.Text), checkBox1.Checked);
+            }
+            catch
+            {
+                f.Show();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(textBox2.Text);
+        }
+    }
+
+}
---
+++ Password Gen/quack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Password_Gen
+{
+    class quack
+    {
+        public static string randomstring(int a, bool ASCII)
+        {
+            string basestring = "abcdefghijklmnopqrstuvwxyz123456789";
+            string basestringASCII = "abcdefghijklmnopqrstuvwxyz123456789.<!@;:#$%^&*()_+-={}[]æ«§ª•¢¹ඞ▌╧░│";
+            string randomstring = "";
+            Random r = new Random();
+            List<string> charlist = new List<string>();
+            if (ASCII == false)
+            {
+                foreach (char f in basestring)
+                {
+                    charlist.Add(f.ToString());
+                }
+                for (int i = 0; i < a; i++)
+                {
+                    randomstring = randomstring + charlist[r.Next(basestring.Length)];
+                }
+            }
+            else
+            {
+                foreach (char f in basestringASCII)
+                {
+                    charlist.Add(f.ToString());
+                }
+                for (int i = 0; i < a; i++)
+                {
+                    randomstring = randomstring + charlist[r.Next(basestringASCII.Length)];
+                }
+            }
+
+
+            return randomstring;
+
+        }
+        public static int randomint(int min, int max)
+        {
+            Random r = new Random();
+            return r.Next(min, max);
+        }
+    }
+}
